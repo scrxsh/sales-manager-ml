b5091c623aee1526c017ac3d36036863a95f3b15
Will ZP <107649183+scrxsh@users.noreply.github.com>
2025-04-16 12:44:05 -0500

    v 0.1.1.alpha

--- back-end/src/main/java/com/example/sales_manager_ml/modules/inventoryManager/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sales_manager_ml.modules.inventoryManager;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<ProductoModel,Long> {
-
-
-
-}
